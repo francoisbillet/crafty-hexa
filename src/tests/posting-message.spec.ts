8553a318ed711817dee98863ee4717f2bbe982a8
François Billet <francois.billet.ext@veolia.com>
2025-01-29 14:06:45 +0100

    use case simple et inversion de dépendances

--- src/tests/posting-message.spec.ts
+++ src/tests/posting-message.spec.ts
@@ -1,3 +1,10 @@
+import {
+  Message,
+  MessageRepository,
+  PostMessageCommand,
+  PostMessageUseCase,
+} from "../post-message.usecase";
+
 describe("Feature: Posting a message", () => {
   describe("Rule: A message can contain a maximum of 280 characters", () => {
     test("Alice can post a new message on her timeline", async () => {
@@ -19,21 +26,43 @@ describe("Feature: Posting a message", () => {
   });
 });
 
-function givenNowIs(date: Date) {
-  // ...
+let message: Message;
+
+class InMemoryMessageRepository implements MessageRepository {
+  save(msg: Message): void {
+    message = msg;
+  }
 }
 
-function whenUserPostsMessage(message: {
-  id: string;
-  text: string;
-  author: string;
-}) {
-  // ...
+const messageRepository = new InMemoryMessageRepository();
+
+export interface DateProvider {
+  getNow(): Date;
 }
 
-function thenPostedMessageShouldBe(message: {
-  id: string;
-  text: string;
-  author: string;
-  publishedAt: Date;
-}) {}
+class StubDateProvider implements DateProvider {
+  now: Date;
+
+  getNow(): Date {
+    return this.now;
+  }
+}
+
+const stubDateProvider = new StubDateProvider();
+
+const postMessageUseCase = new PostMessageUseCase(
+  messageRepository,
+  stubDateProvider
+);
+
+function givenNowIs(_now: Date) {
+  stubDateProvider.now = _now;
+}
+
+function whenUserPostsMessage(postMessage: PostMessageCommand) {
+  postMessageUseCase.handle(postMessage);
+}
+
+function thenPostedMessageShouldBe(expectedMessage: Message) {
+  expect(expectedMessage).toEqual(message);
+}
